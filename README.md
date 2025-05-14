2952ca0830f154f1eda89b0865bfdbd78e4d7e5a
Olavi Sau <olavi.sau@betssongroup.com>
2025-05-15 00:59:46 +0300

    feat: simplify and add proper exclusion list, use ascii and levenshtein

--- README.md
+++ README.md
@@ -5,8 +5,15 @@ This is a small evaluation project for Sensor Tower.
 ## How to run?
 1. `npm i`
 2. `npm run build`
-3. `node dist/run.js`
+3. `node dist/run.js > log.txt`
+
+- DEBUG environment variable for additional log output.
+- It might take a second since the comparison includes Levenshtein which is n^2.
+- OUTPUT
+  - companies.unique.txt Companies without detected duplicates.
+  - companies.duplicate.txt Companies found to have duplicates.
 
 ## Requirements
 - npm (10.8.2 used for development)
 - Node.js (v20.18.0 used for development)
+- Mac environment used for development - your mileage may vary with Windows.
--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,7 @@
-import { getCompanies } from "./get-companies";
+import { getCompanies } from "./get-companies.js";
+import { writeFile } from "node:fs/promises";
+import { distance } from "fastest-levenshtein";
+import ascii from "any-ascii";
 
 (async () => {
     const inputCompanies = await getCompanies();
@@ -6,6 +9,7 @@ import { getCompanies } from "./get-companies";
     const originalCount = lines.length;
     console.log(`INFO: input has ${originalCount} lines`);
 
+    // just making sure I didn't get funny results by using new line to split
     if (process.env.DEBUG) {
         for (const line of lines) {
             if (line.length < 3) {
@@ -17,68 +21,99 @@ import { getCompanies } from "./get-companies";
         console.log(`INFO: logged abnormal lines for error discovery`);
     }
 
-    // build a map of companies for keeping track of duplicates and applies transformations
-    // it might be that some transformations are best applied step by step, but it might also not be necessary
+    // build a map of companies for keeping track of duplicates
     const companies = new Map<string, Company>();
     for (const name of lines) {
         const company = companies.get(name);
         if (!company) {
             companies.set(name, {
-                name: name,
-                currentTransformation: name,
-                transformations: [name],
+                name,
                 duplicates: []
             });
         } else {
             company.duplicates.push(name);
+            // perhaps useful on a different data set
             console.log(`INFO: found exact duplicate ${name}`);
         }
     }
 
+    // transform the company names into their most basic forms
     const transformations = new Map<string, Company>();
     for (const [name, company] of companies) {
-        // each transformation should be tested individually to make sure we don't have too many false positives
-        // it might be that a single transformation is reliable enough
-        const transformation = company
-            .currentTransformation
+        const transformation = ascii(
+            name
+                .replace(/[.,!\-:%©&]/g, "") // remove all special characters
+                .replace(/工作室/g, "")  // chinese word for studio
+                .replace(/株式会社/g, "") // asian holding company
+                .replace(/深圳市/g, "") // Shenzhen
+        )
             .toLowerCase()
-            .replace(/ (llc|ltd|co|limited|studios?|games)([.,])*/g, "")
-            .normalize("NFKD")
-            .replace(/[.,\-:%\s\t]/g, "")
-            .trim();
-        company.currentTransformation = transformation;
-        company.transformations.push(transformation);
+            // common words that are unlikely to cause false positives even without space prefix
+            .replace(/(games?|studios?|limited|collective)/g, "")
+            // generic business terms
+            .replace(/ (llc|ltd|co|team|pictures?|proje[ck]ts?|studis|networks?|arts|vr|interactive|digital|development|oü|oy|productions?|corporation|corp|technology|bv|solutions|works|software|creative|games?|gmaes|gaming|GmbH|inc|designs?|media|group|entertain?e?ment|experience|&amp;?)/g, "")
+            .replace(/[\s\t]/g, ""); // remove spaces and tabs
 
+        // add as duplicate or add as a new "unique" transformation
         const duplicate = transformations.get(transformation);
         if (duplicate !== undefined) {
-            duplicate.duplicates.push(name);
-            company.duplicates.push(duplicate.name);
+            markDuplicate(company, duplicate);
         } else {
             transformations.set(transformation, company);
+            process.env.DEBUG && console.log(transformation); // useful for checking the output of the transformations
         }
     }
 
+    let i = 0;
+    for (const [transformation, company] of transformations) {
+        if (transformation.length < 8) continue; // less than eight has too many false positives
+        i++ % 1000 === 0 ? console.log(`${i} entries compared`) : undefined; // just so progress can be monitored
+        for (const [duplicateTransformation, duplicate] of transformations) {
+            if (duplicateTransformation.length < 8 || duplicate.duplicates.length || company === duplicate) continue;
+            if(
+                // less runs into a decent amount of false positives with human names
+                transformation.length > 11
+                && duplicateTransformation.length > 11
+                && transformation.substring(0, 11) === duplicateTransformation.substring(0, 11)
+            ) {
+                markDuplicate(company, duplicate);
+            } else if (
+                Math.abs(transformation.length - duplicateTransformation.length) < 1
+                && distance(transformation, duplicateTransformation) === 1
+            ) {
+                markDuplicate(company, duplicate);
+            }
+        }
+    }
+
+    const duplicateFile = [];
     const uniqueCompanies = new Set<string>();
     for (const [name, company] of companies) {
         if (!company.duplicates || !company.duplicates.some((duplicate) => uniqueCompanies.has(duplicate))) {
             uniqueCompanies.add(name);
-            if (process.env.DEBUG && company.duplicates.length) {
-                console.log(name);
+            if (company.duplicates.length) {
+                duplicateFile.push(name);
                 for (const duplicate of company.duplicates) {
-                    console.log(`- ${duplicate}`)
+                    duplicateFile.push(`- ${duplicate}`)
                 }
             }
         }
     }
+
+    await writeFile("companies.duplicate.txt", duplicateFile.join("\n"), "utf8");
+    await writeFile("companies.unique.txt", Array.from(uniqueCompanies).join("\n"), "utf8");
 
     console.log(
         `INFO: basic transformations removed ${originalCount - uniqueCompanies.size} companies`
     );
 })();
 
+function markDuplicate(company: Company, duplicate: Company) {
+    company.duplicates.push(duplicate.name);
+    duplicate.duplicates.push(company.name);
+}
+
 interface Company {
     name: string;
-    currentTransformation: string,
-    transformations: string[],
     duplicates: string[]
 }
